04983040fbbcee3ca0326a2931bb547efd6f6e66
Fauqi Amar Hizbullah <a@fauqi.com>
2023-08-08 14:37:45 +0700

    Add GET and DELETE methods in dashboard page

--- es-manage.api/DB/1.0.0/001_CREATE_UserMst.sql
+++ es-manage.api/DB/1.0.0/001_CREATE_UserMst.sql
@@ -1,10 +1,14 @@
 CREATE TABLE IF NOT EXISTS public."usermst" (
     "id" UUID PRIMARY KEY,
     "username" VARCHAR(25) NOT NULL,
-    "displayName" VARCHAR(50),
+    "displayname" VARCHAR(50),
     "password" VARCHAR(250) NOT NULL,
-    "createdOn" TIMESTAMP,
-    "createdBy" VARCHAR(25),
-    "modifiedOn" TIMESTAMP,
-    "modifiedBy" VARCHAR(25)
+    "createdon" TIMESTAMP,
+    "createdby" VARCHAR(25),
+    "modifiedon" TIMESTAMP,
+    "modifiedby" VARCHAR(25),
+    "deletedat" TIMESTAMP
 );
+
+ALTER TABLE IF EXISTS public."usermst"
+    OWNER to es;
