88b9658ab0b3cf7faa00b0fd5afbe6b179df98b8
Knoxberg <jasirihsan@gmail.com>
2023-08-16 09:02:15 +0700

    Add ItemSupplier Endpoint

--- es-manage.api/Program.cs
+++ es-manage.api/Program.cs
@@ -46,6 +46,9 @@ builder.Services.AddScoped<UserRepository>();
 builder.Services.AddScoped<AuthRepository>();
 builder.Services.AddScoped<ItemDepartmentRepository>();
 builder.Services.AddScoped<ItemRepository>();
+builder.Services.AddScoped<ItemSupplierRepository>();
+builder.Services.AddScoped<ItemSupplier_TransactionRepository>();
+builder.Services.AddScoped<SupplierRepository>();
 builder.Services.AddScoped<BrandRepository>();
 builder.Services.AddScoped<TokenService>();
 
