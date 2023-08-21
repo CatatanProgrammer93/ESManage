e349374a4435d5b8024035be21fdf050fc95793a
Fauqi Amar Hizbullah <a@fauqi.com>
2023-08-21 09:53:00 +0700

    Fix DELETE method in all show pages

--- es-manage/src/Brand/Show.jsx
+++ es-manage/src/Brand/Show.jsx
@@ -1,23 +1,23 @@
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import logo from '../assets/logo.svg';
-import dashboard from '../assets/dashboard.svg';
-import recent from '../assets/recent.svg';
-import alert from '../assets/alert.svg';
-import asset from '../assets/asset.svg';
-import userIcon from '../assets/user.svg';
-import setting from '../assets/setting.svg';
-import '../App.css';
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg";
+import dashboard from "../assets/dashboard.svg";
+import recent from "../assets/recent.svg";
+import alert from "../assets/alert.svg";
+import asset from "../assets/asset.svg";
+import userIcon from "../assets/user.svg";
+import setting from "../assets/setting.svg";
+import "../App.css";
 
 function Dashboard() {
   const [brands, setBrands] = useState([]);
   const location = useLocation();
   const userFromState = location.state ? location.state.user : null;
-  const userFromStorageItem = localStorage.getItem('user');
+  const userFromStorageItem = localStorage.getItem("user");
   let userFromStorage = null;
   try {
-    if (userFromStorageItem && userFromStorageItem !== 'undefined') {
+    if (userFromStorageItem && userFromStorageItem !== "undefined") {
       userFromStorage = JSON.parse(userFromStorageItem);
     }
   } catch (e) {
@@ -26,11 +26,11 @@ function Dashboard() {
   }
 
   const user = userFromState || userFromStorage;
-  const displayName = user ? user.displayName : 'Guest';
+  const displayName = user ? user.displayName : "Guest";
 
-  const deleteBrand = (id, categoryName) => {
-    fetch(`https://localhost:7240/api/brand/${id}/${categoryName}`, {
-      method: 'DELETE',
+  const deleteBrand = (id) => {
+    fetch(`https://localhost:7240/api/brand/${id}`, {
+      method: "DELETE",
     }).then(() => {
       setBrands(brands.filter((brand) => brand.id !== id));
     });
@@ -38,7 +38,7 @@ function Dashboard() {
 
   useEffect(() => {
     // Fetch Item Departments
-    fetch('https://localhost:7240/api/brand')
+    fetch("https://localhost:7240/api/brand")
       .then((res) => res.json())
       .then((data) => setBrands(data));
   }, []);
@@ -51,8 +51,8 @@ function Dashboard() {
           <ul className="">
             <Link to="/dashboard">
               <li className="inline-flex items-center py-6">
-              <img src={dashboard} alt="dashboard" className="w-8" />
-              <span className="text-white font-medium ml-2">Dashboard</span>
+                <img src={dashboard} alt="dashboard" className="w-8" />
+                <span className="text-white font-medium ml-2">Dashboard</span>
               </li>
             </Link>
             <br />
@@ -68,7 +68,9 @@ function Dashboard() {
             <br />
             <li className="inline-flex items-center py-6">
               <img src={recent} alt="recent" className="w-8" />
-              <span className="text-white font-medium ml-2 ">Recent Activities</span>
+              <span className="text-white font-medium ml-2 ">
+                Recent Activities
+              </span>
             </li>
             <br />
             <li className="inline-flex items-center py-6">
@@ -82,9 +84,15 @@ function Dashboard() {
         <div className="absolute top-2 left-96">
           <div className="mt-16 inline-flex items-center">
             <form action="">
-              <input type="text" className="bg-white text- text-lg text-black bg-transparent border-none rounded-xl py-3 px-8 w-[50rem] mr-32" placeholder="Search" />
+              <input
+                type="text"
+                className="bg-white text- text-lg text-black bg-transparent border-none rounded-xl py-3 px-8 w-[50rem] mr-32"
+                placeholder="Search"
+              />
             </form>
-            <span className="text-white font-semibold text-xl mx-4">{displayName}</span>
+            <span className="text-white font-semibold text-xl mx-4">
+              {displayName}
+            </span>
             <img src={userIcon} alt="user" className="w-[52px]" />
           </div>
 
@@ -92,7 +100,9 @@ function Dashboard() {
           <div className="overflow-x-auto mt-20">
             <h1 className="text-white mb-4 font-bold text-2xl">Brand</h1>
             <Link to="/brand/create">
-              <button className="btn text-quaternary font-semibold mb-5">Create new</button>
+              <button className="btn text-quaternary font-semibold mb-5">
+                Create new
+              </button>
             </Link>
             <table className="table">
               <thead className="text-white text-lg">
@@ -115,9 +125,14 @@ function Dashboard() {
                     <th></th>
                     <td>
                       <Link to={`/brand/edit/${brand.id}`}>
-                        <button className="btn text-quaternary font-semibold mr-2">Edit</button>
+                        <button className="btn text-quaternary font-semibold mr-2">
+                          Edit
+                        </button>
                       </Link>
-                      <button className="btn bg-red-700 text-white font-semibold border-none" onClick={() => deleteBrand(brand.id)}>
+                      <button
+                        className="btn bg-red-700 text-white font-semibold border-none"
+                        onClick={() => deleteBrand(brand.id)}
+                      >
                         Delete
                       </button>
                     </td>
--- es-manage/src/ItemSupplierTransaction/Show.jsx
+++ es-manage/src/ItemSupplierTransaction/Show.jsx
@@ -1,23 +1,23 @@
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import logo from '../assets/logo.svg';
-import dashboard from '../assets/dashboard.svg';
-import recent from '../assets/recent.svg';
-import alert from '../assets/alert.svg';
-import asset from '../assets/asset.svg';
-import userIcon from '../assets/user.svg';
-import setting from '../assets/setting.svg';
-import '../App.css';
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg";
+import dashboard from "../assets/dashboard.svg";
+import recent from "../assets/recent.svg";
+import alert from "../assets/alert.svg";
+import asset from "../assets/asset.svg";
+import userIcon from "../assets/user.svg";
+import setting from "../assets/setting.svg";
+import "../App.css";
 
 function Dashboard() {
   const [itemsuppliertransactions, setItemSupplierTransactions] = useState([]);
   const location = useLocation();
   const userFromState = location.state ? location.state.user : null;
-  const userFromStorageItem = localStorage.getItem('user');
+  const userFromStorageItem = localStorage.getItem("user");
   let userFromStorage = null;
   try {
-    if (userFromStorageItem && userFromStorageItem !== 'undefined') {
+    if (userFromStorageItem && userFromStorageItem !== "undefined") {
       userFromStorage = JSON.parse(userFromStorageItem);
     }
   } catch (e) {
@@ -26,21 +26,25 @@ function Dashboard() {
   }
 
   const user = userFromState || userFromStorage;
-  const displayName = user ? user.displayName : 'Guest';
+  const displayName = user ? user.displayName : "Guest";
 
   const deleteItemSupplierTransaction = (id) => {
     fetch(`https://localhost:7240/api/itemsupplier_transaction/${id}`, {
-      method: 'DELETE',
+      method: "DELETE",
     }).then(() => {
-      setItemSupplierTransactions(itemsuppliertransactions.filter((itemsuppliertransaction) => itemsuppliertransaction.id !== id));
+      setItemSupplierTransactions(
+        itemsuppliertransactions.filter(
+          (itemsuppliertransaction) => itemsuppliertransaction.id !== id
+        )
+      );
     });
   };
 
   useEffect(() => {
     // Fetch ItemsSupplierTransaction
-    fetch('https://localhost:7240/api/itemsupplier_transaction')
-    .then((res) => res.json())
-    .then((data) => setItemSupplierTransactions(data));
+    fetch("https://localhost:7240/api/itemsupplier_transaction")
+      .then((res) => res.json())
+      .then((data) => setItemSupplierTransactions(data));
   }, []);
 
   return (
@@ -51,8 +55,8 @@ function Dashboard() {
           <ul className="">
             <Link to="/dashboard">
               <li className="inline-flex items-center py-6">
-              <img src={dashboard} alt="dashboard" className="w-8" />
-              <span className="text-white font-medium ml-2">Dashboard</span>
+                <img src={dashboard} alt="dashboard" className="w-8" />
+                <span className="text-white font-medium ml-2">Dashboard</span>
               </li>
             </Link>
             <br />
@@ -68,7 +72,9 @@ function Dashboard() {
             <br />
             <li className="inline-flex items-center py-6">
               <img src={recent} alt="recent" className="w-8" />
-              <span className="text-white font-medium ml-2 ">Recent Activities</span>
+              <span className="text-white font-medium ml-2 ">
+                Recent Activities
+              </span>
             </li>
             <br />
             <li className="inline-flex items-center py-6">
@@ -82,17 +88,27 @@ function Dashboard() {
         <div className="absolute top-2 left-96">
           <div className="mt-16 inline-flex items-center">
             <form action="">
-              <input type="text" className="bg-white text- text-lg text-black bg-transparent border-none rounded-xl py-3 px-8 w-[50rem] mr-32" placeholder="Search" />
+              <input
+                type="text"
+                className="bg-white text- text-lg text-black bg-transparent border-none rounded-xl py-3 px-8 w-[50rem] mr-32"
+                placeholder="Search"
+              />
             </form>
-            <span className="text-white font-semibold text-xl mx-4">{displayName}</span>
+            <span className="text-white font-semibold text-xl mx-4">
+              {displayName}
+            </span>
             <img src={userIcon} alt="user" className="w-[52px]" />
           </div>
 
           {/* Item Supplier Transaction table */}
           <div className="overflow-x-auto mt-20">
-            <h1 className="text-white mb-4 font-bold text-2xl">Item Supplier Transaction</h1>
+            <h1 className="text-white mb-4 font-bold text-2xl">
+              Item Supplier Transaction
+            </h1>
             <Link to="/item-supplier-transaction/create">
-              <button className="btn text-quaternary font-semibold mb-5">Create new</button>
+              <button className="btn text-quaternary font-semibold mb-5">
+                Create new
+              </button>
             </Link>
             <table className="table">
               <thead className="text-white text-lg">
@@ -107,24 +123,37 @@ function Dashboard() {
                 </tr>
               </thead>
               <tbody className="text-white text-lg">
-                {itemsuppliertransactions.map((itemsuppliertransaction, index) => (
-                  <tr key={index}>
-                    <th>{index + 1}</th>
-                    <td>{itemsuppliertransaction.itemSupplierId}</td>
-                    <td>{itemsuppliertransaction.transactionType}</td>
-                    <td>{itemsuppliertransaction.transactionDate}</td>
-                    <td>{itemsuppliertransaction.quantity}</td>
-                    <td>{itemsuppliertransaction.notes}</td>
-                    <td>
-                      <Link to={`/item-supplier-transaction/edit/${itemsuppliertransaction.id}`}>
-                        <button className="btn text-quaternary font-semibold mr-2">Edit</button>
-                      </Link>
-                      <button className="btn bg-red-700 text-white font-semibold border-none" onClick={() => deleteItemSupplierTransaction(itemsuppliertransaction.id)}>
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                ))}
+                {itemsuppliertransactions.map(
+                  (itemsuppliertransaction, index) => (
+                    <tr key={index}>
+                      <th>{index + 1}</th>
+                      <td>{itemsuppliertransaction.itemSupplierId}</td>
+                      <td>{itemsuppliertransaction.transactionType}</td>
+                      <td>{itemsuppliertransaction.transactionDate}</td>
+                      <td>{itemsuppliertransaction.quantity}</td>
+                      <td>{itemsuppliertransaction.notes}</td>
+                      <td>
+                        <Link
+                          to={`/item-supplier-transaction/edit/${itemsuppliertransaction.id}`}
+                        >
+                          <button className="btn text-quaternary font-semibold mr-2">
+                            Edit
+                          </button>
+                        </Link>
+                        <button
+                          className="btn bg-red-700 text-white font-semibold border-none"
+                          onClick={() =>
+                            deleteItemSupplierTransaction(
+                              itemsuppliertransaction.id
+                            )
+                          }
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  )
+                )}
               </tbody>
             </table>
           </div>
