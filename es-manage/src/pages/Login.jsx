2f084333e908ff55ccd21bb2e88cf29eb01cfa21
Fauqi Amar Hizbullah <a@fauqi.com>
2023-11-02 14:14:20 +0700

    Update the login page

--- es-manage/src/pages/Login.jsx
+++ es-manage/src/pages/Login.jsx
@@ -1,12 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import logo from "../assets/logo.svg";
-import ellipse from "../assets/ellipse.svg";
-import ellipse2 from "../assets/ellipse-2.svg";
-import esmanage from "../assets/esmanage.svg";
-import border from "../assets/border.svg";
-import "../App.css";
 
 function Login() {
   const [userName, setUserName] = useState("");
@@ -44,60 +38,65 @@ function Login() {
   };
 
   return (
-    <div className="bg-[#0F0F0F] h-screen">
-      <div className="container">
-        <img src={logo} alt="logo" className="w-20 ml-12 pt-10 max-md:w-16" />
-        <div className="max-md:hidden">
-          <h1 className="text-6xl font-semibold text-white mt-40 ml-24 mb-12">
-            Welcome to <br /> our platform!
-          </h1>
-          <img src={esmanage} alt="esmanage" className="w-40 ml-24" />
-        </div>
-        <div className="flex justify-center text-center max-md:mt-6">
-          <img
-            src={ellipse}
-            alt="ellipse"
-            className="absolute text-center w-52 md:top-[80px] md:left-[765px] max-md:hidden"
-          />
-          <img
-            src={ellipse2}
-            alt="ellipse2"
-            className="absolute text-center w-36 md:top-[550px] md:right-[180px] max-md:hidden"
-          />
-          <img
-            src={border}
-            alt="border"
-            className="absolute text-center backdrop-blur-md w-96 md:top-[80px] md:left-[900px]"
-          />
-        </div>
-        <div className="absolute z-10 md:top-[80px] md:left-[900px] px-12 pt-6 max-md:mt-">
-          <h2 className="text-3xl font-semibold text-white mt-24">Login</h2>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              className="text-xl text-white bg-transparent border rounded-xl py-3 px-8 mt-16"
-              placeholder="Username"
-              value={userName}
-              onChange={(e) => setUserName(e.target.value)}
-            />
-            <br />
-            <input
-              type="password"
-              className="text-xl text-white bg-transparent border rounded-xl py-3 px-8 mt-6"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <br />
-            <button
-              type="submit"
-              className="bg-gradient-to-r from-[#628EFF] to-[#580475] border-none text-xl font-semibold text-white border rounded-xl py-3 px-8 mt-12 text-center"
-              disabled={isLoading}
-            >
-              {isLoading ? "Logging in..." : "Login"}
-            </button>
+    <div className="border-top-wide border-primary d-flex flex-column">
+      <div className="page page-center">
+        <div className="container-tight py-4">
+          <div className="text-center mb-4">
+            <a href="." className="navbar-brand navbar-brand-autodark">
+              <img src="{{ asset('dist/img/logo.svg') }}" height="36" alt="" />
+            </a>
+          </div>
+          <form
+            className="card card-md"
+            onSubmit={handleSubmit}
+            autoComplete="off"
+          >
+            <div className="card-body">
+              <h2 className="card-title text-center mb-4">
+                Login to your account
+              </h2>
+              <div className="mb-3">
+                <label className="form-label">Username</label>
+                <input
+                  className="form-control"
+                  placeholder="Enter username"
+                  value={userName}
+                  onChange={(e) => setUserName(e.target.value)}
+                  required
+                  autoFocus
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">
+                  Password
+                  <span className="form-label-description">
+                    <a href="#">I forgot password</a>
+                  </span>
+                </label>
+                <div className="input-group input-group-flat">
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Enter password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="form-footer">
+                <button type="submit" className="btn btn-primary w-100">
+                  Sign in
+                </button>
+              </div>
+            </div>
           </form>
-          {error && <p className="text-red-500 mt-6">{error}</p>}
+          <div className="text-center text-muted mt-3">
+            Don't have account yet?{" "}
+            <a href="#" tabIndex="-1">
+              Sign up
+            </a>
+          </div>
         </div>
       </div>
     </div>
