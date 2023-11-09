9ff88d8b1ef466cad904390128f71849e29e736c
Knoxberg <jasirihsan@gmail.com>
2023-11-09 11:37:59 +0700

    save progress EP change password

--- es-manage.api/Repositories/AuthRepository.cs
+++ es-manage.api/Repositories/AuthRepository.cs
@@ -7,25 +7,6 @@ using Npgsql;
 
 namespace es_manage.api.Repositories {
     public class AuthRepository {
-        /*
-        // Membuat private readonly AppDbContext dan IDbConnection
-        private readonly AppDbContext _context;
-        private readonly IDbConnection _db;
-
-        // Membuat constructor UserRepository yang menerima parameter AppDbContext dan IConfiguration
-        public AuthRepository(AppDbContext context, IConfiguration configuration)
-        {
-            _context = context;
-            _db = new NpgsqlConnection(configuration.GetConnectionString("Main"));
-        }
-
-        // Membuat metode ValidateUser untuk validasi user saat login
-        public async Task<UserMst?> ValidateUser(string username, string password) {
-            var user = await _db.QuerySingleOrDefaultAsync<UserMst>("SELECT * FROM UserMst WHERE UserName = @UserName AND Password = @Password AND DeletedAt IS NULL", new { UserName = username, Password = password });
-            return user;
-        }
-        */
-
         private readonly AppDbContext _context;
         private readonly IDbConnection _db;
         private readonly HashingService _hashingService; // Add HashingService
