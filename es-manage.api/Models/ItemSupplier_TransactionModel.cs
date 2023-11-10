53994a41f473997f6c30733f6145381ca4984841
FondaRasendria <fonfarasendria@gmail.com>
2023-11-10 10:13:08 +0700

    ItemSupplier_Transaction use ItemId and SupplierId

--- es-manage.api/Models/ItemSupplier_TransactionModel.cs
+++ es-manage.api/Models/ItemSupplier_TransactionModel.cs
@@ -12,4 +12,20 @@ namespace es_manage.api.Models {
         public DateTime? ModifiedOn { get; set; }
         public string ModifiedBy { get; set; } = string.Empty;
     }
+
+    public class ItemSupplier_TransactionPlusItemSupplier
+    {
+        public string ID { get; set; } = string.Empty;
+        public string ItemId { get; set;} = string.Empty;
+        public string SupplierId { get; set; } = string.Empty;
+        public string TransactionType { get; set; } = string.Empty;
+        public DateTime? TransactionDate { get; set; }
+        public decimal Quantity { get; set; }
+        public string Notes { get; set; } = string.Empty;
+        public bool Deleted { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public string CreatedBy { get; set; } = string.Empty;
+        public DateTime? ModifiedOn { get; set; }
+        public string ModifiedBy { get; set; } = string.Empty;
+    }
 }
