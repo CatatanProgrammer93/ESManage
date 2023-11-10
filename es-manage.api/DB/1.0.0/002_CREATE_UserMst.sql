d4289da56185ac1236b2d4d18f3e6b92951a8623
Knoxberg <jasirihsan@gmail.com>
2023-11-10 14:38:22 +0700

    Add starter admin account

--- es-manage.api/DB/1.0.0/002_CREATE_UserMst.sql
+++ es-manage.api/DB/1.0.0/002_CREATE_UserMst.sql
@@ -15,3 +15,31 @@ CREATE TABLE IF NOT EXISTS public."usermst" (
 
 ALTER TABLE IF EXISTS public."usermst"
     OWNER to es;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+INSERT INTO public."usermst" (
+    "id",
+    "username",
+    "displayname",
+    "password",
+    "passwordsalt",
+    "createdon",
+    "createdby",
+    "modifiedon",
+    "modifiedby",
+    "deletedat",
+    "roleid"
+) VALUES (
+    uuid_generate_v4(),
+    'admin',
+    'Default Administrator',
+    'QZelWwn5LIpZ1AJDhRIzSEpkW7Efy1MZAVk3nW2K0fJZ5jcmj8/hQa8QXrFFwPrysYn/2wIu9YioL0KOOBRPkA==',
+    'gTkmXTZrOzbv79+g0vWuNGA7cJ+EBDvyK0asZ5lCA7mi9WnWIdWP4nGWr952QE2AszbWW2+4RvTx6n0FDebXoA==',
+    NOW(),
+    'System',
+    null,
+    null,
+    null,
+    '1'
+);
