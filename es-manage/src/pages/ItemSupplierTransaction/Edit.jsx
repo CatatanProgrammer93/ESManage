0c2698a0060d08534054fa735be98e673df081d2
yeyenfebiola <yeyenfebiola2@gmail.com>
2024-01-08 10:05:20 +0700

    Fix Button

--- es-manage/src/pages/ItemSupplierTransaction/Edit.jsx
+++ es-manage/src/pages/ItemSupplierTransaction/Edit.jsx
@@ -44,9 +44,8 @@ function EditItemSupplierTransaction() {
         const transactionData = transactionResponse.data;
         setId(transactionData.id);
         setItemSupplierId(transactionData.itemSupplierId);
-          setTransactionType(transactionData.transactionType);
-          const formattedDate = new Date(transactionData.transactionDate).toISOString().split('T')[0];
-          setTransactionDate(formattedDate);
+        setTransactionType(transactionData.transactionType);
+        setTransactionDate(transactionData.transactionDate);
         setQuantity(transactionData.quantity);
         setNotes(transactionData.notes);
       } catch (error) {
@@ -166,13 +165,15 @@ function EditItemSupplierTransaction() {
                 ></textarea>
               </div>
             </div>
-            <div className="mb-3">
-              <input type="submit" value="Save" className="btn btn-primary" />
-            </div>
-            <div className="mb-3">
-              <Link to="/item-supplier-transaction" className="btn btn-primary">
-                Cancel
-              </Link>
+            <div style={{ display: 'flex', gap: '10px' }}>
+              <div className="mb-3">
+                <input type="submit" value="Save" className="btn btn-green" />
+              </div>
+              <div className="mb-3">
+                <Link to="/item" className="btn btn-red">
+                  Cancel
+                </Link>
+              </div>
             </div>
           </form>
           {error && (
